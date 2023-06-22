af4188e8223fc7f55bc60fbb69e48f67113f7f65
Mihir Mittal <105881639+mihirm21@users.noreply.github.com>
2023-06-23 00:17:49 +0530

    updated
    Signed-off-by: Mihir Mittal <105881639+mihirm21@users.noreply.github.com>

--- .github/workflows/assign_reviewers.js
+++ .github/workflows/assign_reviewers.js
@@ -1,6 +1,28 @@
 // assigns reviewers based on the category of the [Proposal] issue
 const { Octokit } = require("@octokit/rest");
 
+const org = "open-feature";
+const repo = process.argv[2];
+const prNumber = process.argv[3];
+const category = process.argv[4];
+
+async function assigner(repo, prNumber, team_Slugs_updated) {
+    const octokit = new Octokit({
+        auth: process.env.TOKEN
+    });
+    try {
+        await octokit.pulls.requestReviewers({
+        owner: repo.split("/")[0],
+        repo: repo.split("/")[1],
+        pull_number: prNumber,
+        team_reviewers: team_Slugs_updated
+        });
+        console.log(`Teams assigned successfully.`);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
 async function assignReviewers(org,repo, prNumber, category) {
   const octokit = new Octokit({
     auth: process.env.TOKEN
@@ -8,38 +30,25 @@ async function assignReviewers(org,repo, prNumber, category) {
   try {
     const { data: teams } = await octokit.request('GET /orgs/{org}/teams',({
       headers: {
-        // authorization: auth.token,
         'X-GitHub-Api-Version': '2022-11-28'
       },
       org: org,
     }));
 
-    if(teams.length == 0){
+    if(teams.length === 0){
       console.log(`No team found for the organisation"${org}".`);
       return;
     }
     // assigns the Proposal issue with "SDK" or "Specification" category to all the "sdk-maintainers"
-    if(category === "SDKs" || category === "Specification"){
+    if(["SDKs", "Specification"].includes(category)){
     const team_updated = teams.filter((team) => team.slug.startsWith("sdk") && team.slug.endsWith("maintainers"));
     const team_Slugs_updated = team_updated.map((team) => team.slug);
-    await octokit.pulls.requestReviewers({
-      owner: repo.split("/")[0],
-      repo: repo.split("/")[1],
-      pull_number: prNumber,
-      team_reviewers: team_Slugs_updated
-    });
-    console.log(`Teams assigned successfully.`);
+    assigner(repo, prNumber, team_Slugs_updated);
     } // assigns the Proposal issue with "OpenFeature Operator" or "Flagd" category to all the "cloud-native-maintainers"
-     else if(category == "OpenFeature Operator" || category == "Flagd"){
+     else if(["Flagd", "OpenFeature Operator"].includes(category)){
     const team_updated = teams.filter((team) => team.slug.startsWith("cloud-native") && team.slug.endsWith("maintainers"));
     const team_Slugs_updated = team_updated.map((team) => team.slug);
-    await octokit.pulls.requestReviewers({
-      owner: repo.split("/")[0],
-      repo: repo.split("/")[1],
-      pull_number: prNumber,
-      team_reviewers: team_Slugs_updated
-    });
-    console.log(`Teams assigned successfully.`);
+    assigner(repo, prNumber, team_Slugs_updated);
     }else {
           console.log(`No team found for the category "${category}".`);
         }
@@ -47,9 +56,5 @@ async function assignReviewers(org,repo, prNumber, category) {
     console.error("An error occurred:", error);
   }
 }
-const org = "open-feature";
-const repo = process.argv[2];
-const prNumber = process.argv[3];
-const category = process.argv[4];
 
 assignReviewers(org, repo, prNumber, category);
